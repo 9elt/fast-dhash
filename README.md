856af0f875e3326b5ebfeb23e8c3bdec395b789e
cieffelle <lorenzo.cicuttin@recrowd.com>
2025-01-20 16:35:09 +0100

    removed unnecessary unsafe, improved code readability, better descriptions

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Fast DHash
+# Fast Dhash
 
 A fast rust implementation of the perceptual hash [*dhash*](https://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html).
 
-The main difference with other rust implementations, and the reason it is called *fast*, is that it uses multi-threading and doesn't rely internally on the [*image*](https://docs.rs/image/latest/image/index.html) crate methods.
+The main difference with other rust implementations, and the reason it is called *fast*, is that it uses multithreading and does not resize nor converts the image, effectively cycling through its bytes only once.
 
 ## Usage
+
+For forward and backward compatibility, it does NOT rely directly on the [*image*](https://docs.rs/image/latest/image/index.html) crate.
 
 ```rust
 use fast_dhash::Dhash;
@@ -21,6 +23,7 @@ if let Ok(image) = image {
         image.height(),
         image.color().channel_count(),
     );
+
     println!("hash: {}", hash);
     // hash: d6a288ac6d5cce14
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-//! # Fast DHash
+//! # Fast Dhash
 //!
 //! A fast rust implementation of the perceptual hash [*dhash*](https://www.hackerfactor.com/blog/index.php?/archives/529-Kind-of-Like-That.html).
 //!
-//! The main difference with other rust implementations, and the reason it is called *fast*, is that it uses multi-threading and doesn't rely internally on the [*image*](https://docs.rs/image/latest/image/index.html) crate methods.
+//! The main difference with other rust implementations, and the reason it is called *fast*, is that it uses multithreading and does not resize nor converts the image, effectively cycling through its bytes only once.
 //!
 //! ### Usage
+//!
+//! For forward and backward compatibility, it does NOT rely directly on the [*image*](https://docs.rs/image/latest/image/index.html) crate.
 //!
 //! ```
 //! use fast_dhash::Dhash;
@@ -21,12 +23,13 @@
 //!         image.height(),
 //!         image.color().channel_count(),
 //!     );
+//!
 //!     println!("hash: {}", hash);
 //!     // hash: d6a288ac6d5cce14
 //! }
 //! ```
 use serde::{Deserialize, Serialize};
-use std::{fmt, num, str};
+use std::{fmt, num, str, thread};
 
 #[derive(Debug, Clone, Copy, Deserialize, Serialize)]
 pub struct Dhash {
@@ -35,8 +38,6 @@ pub struct Dhash {
 
 impl Dhash {
     pub fn new(bytes: &[u8], width: u32, heigth: u32, channel_count: u8) -> Self {
-        let mut grid = [[0f64; 9]; 8];
-
         let width = width as usize;
         let heigth = heigth as usize;
         let channel_count = channel_count as usize;
@@ -44,95 +45,11 @@ impl Dhash {
         let cell_width = width / 9;
         let cell_height = heigth / 8;
 
-        if channel_count >= 3 {
-            // NOTE: RGB(A) images implementation
-            std::thread::scope(|s| {
-                let mut handles = Vec::with_capacity(9);
-
-                for y in 0..8 {
-                    handles.push(s.spawn(move || {
-                        let mut row = [0f64; 9];
-
-                        for x in 0..9 {
-                            let from = x * cell_width;
-                            let to = from + cell_width;
-
-                            let mut rs = 0f64;
-                            let mut gs = 0f64;
-                            let mut bs = 0f64;
-
-                            for image_x in from..to {
-                                let from = y * cell_height;
-                                let to = from + cell_height;
-
-                                for image_y in from..to {
-                                    let i = (image_y * width + image_x) * channel_count;
-
-                                    unsafe {
-                                        rs += *bytes.get_unchecked(i) as f64;
-                                        gs += *bytes.get_unchecked(i + 1) as f64;
-                                        bs += *bytes.get_unchecked(i + 2) as f64;
-                                    }
-                                }
-                            }
-
-                            unsafe {
-                                *row.get_unchecked_mut(x) += rs * 0.299 + gs * 0.587 + bs * 0.114;
-                            }
-                        }
-
-                        (y, row)
-                    }));
-                }
-
-                for handle in handles {
-                    let (y, row) = handle.join().unwrap();
-                    grid[y] = row;
-                }
-            });
+        let grid = if channel_count >= 3 {
+            grid_from_rgb(bytes, width, cell_width, cell_height, channel_count)
         } else {
-            // NOTE: Grayscale images implementation
-            std::thread::scope(|s| {
-                let mut handles = Vec::with_capacity(9);
-
-                for y in 0..8 {
-                    handles.push(s.spawn(move || {
-                        let mut row = [0f64; 9];
-
-                        for x in 0..9 {
-                            let from = x * cell_width;
-                            let to = from + cell_width;
-
-                            let mut luma = 0f64;
-
-                            for image_x in from..to {
-                                let from = y * cell_height;
-                                let to = from + cell_height;
-
-                                for image_y in from..to {
-                                    let i = (image_y * width + image_x) * channel_count;
-
-                                    unsafe {
-                                        luma += *bytes.get_unchecked(i) as f64;
-                                    }
-                                }
-                            }
-
-                            unsafe {
-                                *row.get_unchecked_mut(x) += luma;
-                            }
-                        }
-
-                        (y, row)
-                    }));
-                }
-
-                for handle in handles {
-                    let (y, row) = handle.join().unwrap();
-                    grid[y] = row;
-                }
-            });
-        }
+            grid_from_grayscale(bytes, width, cell_width, cell_height, channel_count)
+        };
 
         let mut bits = [false; 64];
 
@@ -187,4 +104,110 @@ impl str::FromStr for Dhash {
             Err(error) => Err(error),
         }
     }
+}
+
+fn grid_from_rgb(
+    bytes: &[u8],
+    width: usize,
+    cell_width: usize,
+    cell_height: usize,
+    channel_count: usize,
+) -> [[f64; 9]; 8] {
+    let mut grid = [[0f64; 9]; 8];
+
+    thread::scope(|s| {
+        let mut handles = Vec::with_capacity(9);
+
+        for y in 0..8 {
+            handles.push(s.spawn(move || {
+                let mut row = [0f64; 9];
+
+                for (x, cell) in row.iter_mut().enumerate() {
+                    let from = x * cell_width;
+                    let to = from + cell_width;
+
+                    let mut rs = 0f64;
+                    let mut gs = 0f64;
+                    let mut bs = 0f64;
+
+                    for image_x in from..to {
+                        let from = y * cell_height;
+                        let to = from + cell_height;
+
+                        for image_y in from..to {
+                            let i = (image_y * width + image_x) * channel_count;
+
+                            unsafe {
+                                rs += *bytes.get_unchecked(i) as f64;
+                                gs += *bytes.get_unchecked(i + 1) as f64;
+                                bs += *bytes.get_unchecked(i + 2) as f64;
+                            }
+                        }
+                    }
+
+                    *cell += rs * 0.299 + gs * 0.587 + bs * 0.114;
+                }
+
+                (y, row)
+            }));
+        }
+
+        for handle in handles {
+            let (y, row) = handle.join().unwrap();
+            grid[y] = row;
+        }
+    });
+
+    grid
+}
+
+fn grid_from_grayscale(
+    bytes: &[u8],
+    width: usize,
+    cell_width: usize,
+    cell_height: usize,
+    channel_count: usize,
+) -> [[f64; 9]; 8] {
+    let mut grid = [[0f64; 9]; 8];
+
+    thread::scope(|s| {
+        let mut handles = Vec::with_capacity(9);
+
+        for y in 0..8 {
+            handles.push(s.spawn(move || {
+                let mut row = [0f64; 9];
+
+                for (x, cell) in row.iter_mut().enumerate() {
+                    let from = x * cell_width;
+                    let to = from + cell_width;
+
+                    let mut luma = 0f64;
+
+                    for image_x in from..to {
+                        let from = y * cell_height;
+                        let to = from + cell_height;
+
+                        for image_y in from..to {
+                            let i = (image_y * width + image_x) * channel_count;
+
+                            unsafe {
+                                luma += *bytes.get_unchecked(i) as f64;
+                            }
+                        }
+                    }
+
+                    *cell += luma;
+                }
+
+                (y, row)
+            }));
+        }
+
+        for handle in handles {
+            let (y, row) = handle.join().unwrap();
+            grid[y] = row;
+        }
+    });
+
+    grid
 }
